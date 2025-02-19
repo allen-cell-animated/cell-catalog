feb15de1a4a20c0d791b71d0d1f4f225cc05441e
Ruge Li <91452427+rugeli@users.noreply.github.com>
2025-02-19 14:49:43 -0800

    Allow multiple genes entry for normal cell lines (#92)
    * allow multiple gene entry
    
    * update the preview to properly display multiple entries
    
    * fix parental line
    
    ---------
    
    Co-authored-by: meganrm <meganrm@gmail.com>

--- src/cms/preview-templates/CellLinePreview.tsx
+++ src/cms/preview-templates/CellLinePreview.tsx
@@ -3,12 +3,16 @@ import { CellLineTemplate } from '../../templates/cell-line'
 import { TemplateProps } from './types';
 
 const CellLinePreview = ({ entry }: TemplateProps) => {
+    const geneEntry = entry.getIn(["data", "gene"]);
+    const gene = geneEntry ?.join ? geneEntry.join(", ") : (geneEntry || "");
+    const tagLocationEntry = entry.getIn(["data", "tag_location"]);
+    const tagLocation = tagLocationEntry ?.join ? tagLocationEntry.join(", ") : (tagLocationEntry || "");
     return (
         <CellLineTemplate
             cellLineId={entry.getIn(["data", "cell_line_id"])}
             cloneNumber={entry.getIn(["data", "clone_number"])}
-            gene={entry.getIn(["data", "gene"])}
-            tagLocation={entry.getIn(["data", "tag_location"])}
+            gene={gene}
+            tagLocation={tagLocation}
             status={entry.getIn(["data", "status"])}
             thumbnail={entry.getIn(["data", "thumbnail_image"])}
         />
