73df04ab8ae67c9829f1ff316440c735bd486a1d
Joe Heffernan <interim17@gmail.com>
2025-03-06 15:58:07 -0800

    apply unique class to sanger sequencing diagrams

--- src/components/SubPage/GenomicCharacterization.tsx
+++ src/components/SubPage/GenomicCharacterization.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import DiagramCard from "../shared/DiagramCard";
-import { GenomicCharacterizationData as GenomicCharacterizationSubpageProps } from "../../component-queries/types";
 import { Flex } from "antd";
+import classNames from "classnames";
+import { GenomicCharacterizationData as GenomicCharacterizationSubpageProps } from "../../component-queries/types";
+import DiagramCard from "../shared/DiagramCard";
 
 const {
     container,
     card,
+    fullWidth,
 } = require("../../style/genomic-characterization.module.css");
 
 const GenomicCharacterizationSubpage: React.FC<GenomicCharacterizationSubpageProps> = ({
     diagrams,
 }) => {
+
+    diagrams?.forEach((diagram, index) => {
+        console.log(diagram.title);
+    }
+    );
+
     return (
         <Flex
             className={container}
@@ -22,7 +30,10 @@ const GenomicCharacterizationSubpage: React.FC<GenomicCharacterizationSubpagePro
             {diagrams?.map((diagram, index) => (
                 <DiagramCard
                     key={index}
-                    className={card}
+                    className={classNames(
+                        card,
+                        diagram.title.includes("Sanger") ? fullWidth : ""
+                    )}
                     title={diagram.title}
                     caption={diagram.caption}
                     image={diagram.image.childImageSharp.gatsbyImageData}
