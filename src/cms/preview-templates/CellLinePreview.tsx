2af491aea73e0589fb5a6a6400dc67e403f1881a
Ruge Li <rugeli0605@gmail.com>
2025-04-22 15:14:27 -0700

    first draft

--- src/cms/preview-templates/CellLinePreview.tsx
+++ src/cms/preview-templates/CellLinePreview.tsx
@@ -1,25 +1,84 @@
 import React from "react";
-import { CellLineTemplate } from "../../templates/cell-line";
 import { TemplateProps } from "./types";
 import useDisableWheel from "../hooks/useDisableWheel";
+import InfoPanel from "../../components/shared/InfoPanel";
+import ProgressPreview from "./ProgressPreview";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const CellLinePreview = ({ entry }: TemplateProps) => {
+    useDisableWheel();
     const geneEntry = entry.getIn(["data", "gene"]);
     const gene = geneEntry?.join ? geneEntry.join(", ") : geneEntry || "";
+    // tempoarily getting allele count as a string before type fixes
+    const taggedAlleles = entry.getIn(["data", "allele_count"]);
+    // const taggedAllelesEntry = entry.getIn(["data", "allele_count"]);
+    // const taggedAlleles = taggedAllelesEntry?.join ? taggedAllelesEntry.join(", ") : taggedAllelesEntry || "";
     const tagLocationEntry = entry.getIn(["data", "tag_location"]);
     const tagLocation = tagLocationEntry?.join
         ? tagLocationEntry.join(", ")
         : tagLocationEntry || "";
-    useDisableWheel();
+    const fluorescentTagEntry = entry.getIn(["data", "fluorescent_tag"]);
+    const fluorescentTag = fluorescentTagEntry?.join
+        ? fluorescentTagEntry.join(", ")
+        : fluorescentTagEntry || "";
+
+    const status = entry.getIn(["data", "status"]);
+    const thumbnail = entry.getIn(["data", "thumbnail_image"])
+    const thumbnailImage = getImage(thumbnail);
+    const data = [
+        {
+            key: "cell_line_id",
+            label: "Cell Collection ID",
+            children: entry.getIn(["data", "cell_line_id"]),
+        },
+        {
+            key: "protein",
+            label: "Protein",
+            children: entry.getIn(["data", "protein"]),
+        },
+        {
+            key: "gene_symbol",
+            label: "Gene Symbol",
+            children: gene,
+        },
+        {
+            key: "clone_number",
+            label: "Clone Number",
+            children: entry.getIn(["data", "clone_number"]),
+        },
+        {
+            key: "tagged_alleles",
+            label: "Tagged Alleles",
+            children: taggedAlleles,
+        },
+        {
+            key: "structure",
+            label: "Structure",
+            children: entry.getIn(["data", "structure"]),
+        },
+        {
+            key: "fluorescent_tag",
+            label: "Fluorescent Tag",
+            children: fluorescentTag,
+        },
+        {
+            key: "tag_location",
+            label: "Tag Location",
+            children: tagLocation,
+        },
+    ]
     return (
-        <CellLineTemplate
-            cellLineId={entry.getIn(["data", "cell_line_id"])}
-            cloneNumber={entry.getIn(["data", "clone_number"])}
-            gene={gene}
-            tagLocation={tagLocation}
-            status={entry.getIn(["data", "status"])}
-            thumbnail={entry.getIn(["data", "thumbnail_image"])}
-        />
+        <>
+            <ProgressPreview status={status} />
+            <InfoPanel data={data} />
+            {/* troubleshoot thumbnailImage */}
+            {thumbnailImage && (
+                <GatsbyImage
+                    image={thumbnailImage}
+                    alt="Cell Line Thumbnail"
+                />
+            )}
+        </>
     );
 };
 
