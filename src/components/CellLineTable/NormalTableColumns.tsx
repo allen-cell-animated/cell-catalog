7a907a23e1da1622f309ca7c7d0f52a9fbd33987
Joe Heffernan <interim17@gmail.com>
2025-05-15 09:12:06 -0700

    Feature/table sort (#162)
    * basic sort functionality for cell catalog table
    
    * cell table sort
    
    * add typing to table sort handler
    
    * fix undefined sort step bug
    
    * provide full sort directions for all columns
    
    * remove sort behavior from pending lines and disease table
    
    * prevent hover interaction on pending cell line table
    
    * use antd for state
    
    * make it white still
    
    * remove sorters for in progress
    
    * fix class name
    
    * remove log
    
    * strip sorted off inProgress columns in CellLineTable
    
    ---------
    
    Co-authored-by: meganrm <meganrm@gmail.com>

--- src/components/CellLineTable/NormalTableColumns.tsx
+++ src/components/CellLineTable/NormalTableColumns.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Flex, GetProp, Table } from "antd";
-import Icon from "@ant-design/icons";
+import { SortOrder } from "antd/es/table/interface";
 
 import {
     UnpackedGene,
@@ -13,20 +12,47 @@ import {
     certificateOfAnalysisColumn,
     obtainLineColumn,
 } from "./SharedColumns";
+import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+import { RAIN_SHADOW, SERIOUS_GRAY } from "../../style/theme";
 
 const { lastColumn } = require("../../style/table.module.css");
+
+const caseInsensitiveStringCompare = (a = "", b = "") =>
+    a.localeCompare(b, undefined, { sensitivity: "base" });
+
+const sortIcon = ({ sortOrder }: { sortOrder: SortOrder }) => {
+    if (sortOrder === null) {
+        return (
+            <CaretDownOutlined style={{ color: RAIN_SHADOW, fontSize: 16 }} />
+        );
+    }
+    return sortOrder === "ascend" ? (
+        <CaretDownOutlined style={{ color: SERIOUS_GRAY, fontSize: 16 }} />
+    ) : (
+        <CaretUpOutlined style={{ color: SERIOUS_GRAY, fontSize: 16 }} />
+    );
+};
 
 export const getNormalTableColumns = (
     inProgress: boolean
 ): CellLineColumns<UnpackedNormalCellLine> => {
     const columns = [
-        { ...cellLineIdColumn },
+        {
+            ...cellLineIdColumn,
+            sorter: (a: any, b: any) =>
+                (a.cellLineId ?? 0) - (b.cellLineId ?? 0),
+            sortIcon: sortIcon,
+            defaultSortOrder: "ascend" as SortOrder,
+        },
         {
             title: "Protein",
             key: "protein",
             dataIndex: "protein",
             width: 200,
             responsive: mdBreakpoint,
+            sortIcon: sortIcon,
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(a.protein, b.protein),
         },
         {
             title: "Gene Symbol & Name",
@@ -38,27 +64,26 @@ export const getNormalTableColumns = (
                 return (
                     <>
                         {taggedGene.map((gene, index) => (
-                            <GeneDisplay
-                                key={index}
-                                gene={gene}
-                            />
+                            <GeneDisplay key={index} gene={gene} />
                         ))}
                     </>
-                ) 
-                
+                );
             },
-        },
-        {
-            title: "Clone",
-            key: "cloneNumber",
-            dataIndex: "cloneNumber",
-            responsive: mdBreakpoint,
+            sortIcon: sortIcon,
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(
+                    a.taggedGene[0].name,
+                    b.taggedGene[0].name
+                ),
         },
         {
             title: "Tagged Alleles",
             key: "alleleCount",
             dataIndex: "alleleCount",
             responsive: mdBreakpoint,
+            sortIcon: sortIcon,
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(a.alleleCount, b.alleleCount),
         },
         {
             title: "Structure",
@@ -66,6 +91,10 @@ export const getNormalTableColumns = (
             width: 280,
             dataIndex: "structure",
             responsive: mdBreakpoint,
+            sortIcon: sortIcon,
+
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(a.structure, b.structure),
         },
         {
             title: "Fluorescent Tag",
@@ -75,6 +104,13 @@ export const getNormalTableColumns = (
             render: (fluorescentTag: string[]) => {
                 return fluorescentTag?.join(" / ");
             },
+            sortIcon: sortIcon,
+
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(
+                    (a.fluorescentTag ?? []).join("|"),
+                    (b.fluorescentTag ?? []).join("|")
+                ),
         },
         {
             title: "Tag Location",
@@ -85,9 +121,15 @@ export const getNormalTableColumns = (
             render: (tagLocation: string[]) => {
                 return tagLocation?.join(" / ");
             },
+            sortIcon: sortIcon,
+            sorter: (a: any, b: any) =>
+                caseInsensitiveStringCompare(
+                    (a.tagLocation ?? []).join("|"),
+                    (b.tagLocation ?? []).join("|")
+                ),
         },
     ];
-
+    // if active add the buttons
     if (!inProgress) {
         return [
             ...columns,
