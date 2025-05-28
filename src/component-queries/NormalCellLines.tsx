47cf8b2ce0daeccb2022764ac56d172ee41053da
Joe Heffernan <interim17@gmail.com>
2025-05-28 09:34:45 -0700

    use unfixed gatsby image to position and size thumbnail in id column

--- src/component-queries/NormalCellLines.tsx
+++ src/component-queries/NormalCellLines.tsx
@@ -94,7 +94,6 @@ export default function NormalCellLines() {
                                         childImageSharp {
                                             gatsbyImageData(
                                                 placeholder: BLURRED
-                                                layout: FIXED
                                             )
                                         }
                                     }
