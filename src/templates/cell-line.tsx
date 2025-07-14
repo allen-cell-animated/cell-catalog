d79f8c55df19ba28614850a1ba4fd8093745a4eb
Joe Heffernan <interim17@gmail.com>
2025-07-14 14:21:14 -0700

    use unpacked cell line data to populate sub page

--- src/templates/cell-line.tsx
+++ src/templates/cell-line.tsx
@@ -2,63 +2,50 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import { GeneticModification } from "../component-queries/types";
+import { NormalCellLineFrontmatter, UnpackedNormalCellLine } from "../component-queries/types";
+import { convertFrontmatterToNormalCellLines } from "../component-queries/convert-data";
 
 const { container, section } = require("../style/cell-line.module.css");
 
 interface QueryResult {
     data: {
         markdownRemark: {
-            frontmatter: {
-                cell_line_id: string;
-                clone_number: number;
-                gene: string[];
-                genetic_modifications?: GeneticModification[];
-                status: string;
-                thumbnail_image: any;
+            id: string;
+            key: string;
+            fields: {
+                slug: string;
             };
-        };
+            frontmatter: NormalCellLineFrontmatter;
+            };
     };
 }
 
-interface CellLineProps {
-    cellLineId: string;
-    cloneNumber: number;
-    geneticModifications?: GeneticModification[];
-    status: string;
-    thumbnail: any;
+interface CellLineProps extends UnpackedNormalCellLine {
+    href: string;
 }
 
 // eslint-disable-next-line
 export const CellLineTemplate = ({
     cellLineId,
     cloneNumber,
-    geneticModifications,
+    tagLocation,
+    taggedGene,
     status,
-    thumbnail,
+    thumbnailImage,
+    href,
 }: CellLineProps) => {
-    const image = getImage(thumbnail);
+    const image = thumbnailImage ? getImage(thumbnailImage) : undefined;
     return (
         <div className={container}>
             <div className={section}>
                 <h1>AICS-{cellLineId}</h1>
+                <p>Share: {href}</p>
                 <p>Clone Number: {cloneNumber}</p>
-                <p>
-                    Gene:{" "}
-                    {geneticModifications
-                        ?.map((mod) => mod.gene?.frontmatter?.symbol)
-                        .join(" / ")}
-                </p>
-                <p>
-                    Tag:{" "}
-                    {geneticModifications
-                        ?.map((mod) => mod.tag_location)
-                        .join(" / ")}
-                </p>
+                <p>Gene: {taggedGene?.map((gene) => gene.name).join(" / ")}</p>
+                <p>Tag: {tagLocation.join(" / ")}</p>
                 <p>Status: {status}</p>
             </div>
             <div className={section}>
-                <h1>Images</h1>
                 <p>Thumbnail:</p>
                 {image && (
                     <GatsbyImage image={image} alt="Cell Line Thumbnail" />
@@ -70,16 +57,16 @@ export const CellLineTemplate = ({
 
 const CellLine = ({ data }: QueryResult) => {
     const { markdownRemark: cellLine } = data;
+
+    const unpackedCellLine = convertFrontmatterToNormalCellLines({
+        node: cellLine,
+    });
+
     return (
         <Layout>
             <CellLineTemplate
-                cellLineId={cellLine.frontmatter.cell_line_id}
-                cloneNumber={cellLine.frontmatter.clone_number}
-                geneticModifications={
-                    cellLine.frontmatter.genetic_modifications
-                }
-                status={cellLine.frontmatter.status}
-                thumbnail={cellLine.frontmatter.thumbnail_image}
+                href={location.href || ""}
+                {...unpackedCellLine}
             />
         </Layout>
     );
@@ -91,8 +78,36 @@ export const pageQuery = graphql`
     query CellLineByID($id: String!) {
         markdownRemark(id: { eq: $id }) {
             id
+            fields {
+                slug
+            }
             frontmatter {
                 cell_line_id
+                parental_line {
+                    frontmatter {
+                        cell_line_id
+                        clone_number
+                        thumbnail_image {
+                            childImageSharp {
+                                gatsbyImageData(
+                                    placeholder: BLURRED
+                                    layout: FIXED
+                                )
+                            }
+                        }
+                        genetic_modifications {
+                            gene {
+                                frontmatter {
+                                    symbol
+                                    name
+                                }
+                            }
+                            allele_count
+                            tag_location
+                            fluorescent_tag
+                        }
+                    }
+                }
                 clone_number
                 genetic_modifications {
                     gene {
