cbf2134239f6619770e91b64430d701a894b55bd
meganrm <meganrm@gmail.com>
2025-12-03 15:39:00 -0800

    change file format

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [require("@trivago/prettier-plugin-sort-imports")],
-    printWidth: 80,
-    tabWidth: 4,
-    importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-    importOrderGroupNamespaceSpecifiers: true,
-};
